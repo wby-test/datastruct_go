c22ff1d13d04684fc5ff33ea3f78ff9f525f76b3
wby0701 <2829926173@qq.com>
2019-02-20 09:54:09 +0800

    change to ubuntu

---
+++ link/link.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	data int
+	Node *Node
+}
+
+//1、给定链表头指针和一个节点指针在O(1)时间删除该节点
+func deleteRandomNode(cur *Node) {
+
+}
+func main() {
+	fmt.Println("make data struct perfect")
+}
